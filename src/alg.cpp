2f1e5b6594eb12cbb10a96dc7ae2ea80df1a6349
ardaelam <artem.elesin2007@gmail.com>
2026-03-24 21:57:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,52 +1,55 @@
 // Copyright 2025 NNTU-CS
-#include <cstdint>
 #include "alg.h"
+#include <cstdint>
 
 uint64_t collatzMaxValue(uint64_t num) {
-    uint64_t maxVal = num;
-    while (num != 1) {
-        if (num % 2 == 0) {
-            num /= 2;
-        } else {
-            num = 3 * num + 1;
-        }
-        if (num > maxVal) {
-            maxVal = num;
-        }
+  uint64_t maxVal = num;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
     }
-    return maxVal;
+    if (num > maxVal) {
+      maxVal = num;
+    }
+  }
+  return maxVal;
 }
 
 unsigned int collatzLen(uint64_t num) {
-    unsigned int len = 1;
-    while (num != 1) {
-        if (num % 2 == 0) {
-            num /= 2;
-        } else {
-            num = 3 * num + 1;
-        }
-        ++len;
+  unsigned int len = 1;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
     }
-    return len;
+    ++len;
+  }
+  return len;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
-    if (lbound > rbound) {
-        if (maxlen) *maxlen = 0;
-        return 0;
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound,
+                        uint64_t rbound) {
+  if (lbound > rbound) {
+    if (maxlen)
+      *maxlen = 0;
+    return 0;
+  }
+
+  unsigned int bestLen = 0;
+  uint64_t bestNum = lbound;
+
+  for (uint64_t i = lbound; i <= rbound; ++i) {
+    unsigned int len = collatzLen(i);
+    if (len > bestLen) {
+      bestLen = len;
+      bestNum = i;
     }
+  }
 
-    unsigned int bestLen = 0;
-    uint64_t bestNum = lbound;
-
-    for (uint64_t i = lbound; i <= rbound; ++i) {
-        unsigned int len = collatzLen(i);
-        if (len > bestLen) {
-            bestLen = len;
-            bestNum = i;
-        }
-    }
-
-    if (maxlen) *maxlen = bestLen;
-    return static_cast<unsigned int>(bestNum);
+  if (maxlen)
+    *maxlen = bestLen;
+  return static_cast<unsigned int>(bestNum);
 }
